35cf4952532970e7296af26bc50d5f5d8cc436d4
Brian Starkey <stark3y@gmail.com>
2020-07-04 16:27:46 +0100

    Various updates
    fzf, cscope, openscad highlight, gitignore

--- install.sh
+++ install.sh
@@ -10,13 +10,27 @@ for i in `find $CURDIR/vim/plugin -name "*.vim"`; do
 	fi
 	ln -s $i `echo $i | sed "s;.*/vim/plugin;$VIMPLUGIN;g"`
 done
+mkdir -p $HOME/.vim/custom
+ln -s $CURDIR/vim/custom/cscope_maps.vim $HOME/.vim/custom/cscope_maps.vim
+ln -s $CURDIR/vim/custom/fzf_maps.vim $HOME/.vim/custom/fzf_maps.vim
+
+echo "Install junegunn/vim-plug"
+echo "Remember to run :PlugInstall"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Install OpenSCAD highlight"
+git clone https://github.com/sirtaj/vim-openscad.git /tmp/vim-openscad
+cp -vr /tmp/vim-openscad/* $HOME/.vim/
+rm -rf /tmp/vim-openscad
 
 echo "Installing tmux settings..."
 ln -s $CURDIR/clip ~/bin/clip
 ln -s $CURDIR/tmux.conf ~/.tmux.conf
 
 echo "Installing git settings..."
-ln -s $CURDIR/gitignore_global ~/.gitignore_global
+ln -s $CURDIR/gitignore_global $HOME/.gitignore_global
+git config --global core.excludesFile $HOME/.gitignore_global
 
 echo "Installing swapcaps..."
 ln -s $CURDIR/swapcaps ~/bin/swapcaps
---
+++ vim/custom/fzf_maps.vim
@@ -0,0 +1,4 @@
+
+map <C-J> :Buffers<CR>
+map <C-H> :Files<CR>
+map <C-K> :Rg <C-R>=expand("<cword>")<CR><CR>
