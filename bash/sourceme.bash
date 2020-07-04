a23301f9e4adafbac248b131e157874074927d23
Brian Starkey <stark3y@gmail.com>
2020-07-04 16:40:13 +0100

    bash: Move ps1 and add common sourceme

---
+++ bash/sourceme.bash
@@ -0,0 +1,17 @@
+
+# http://stackoverflow.com/a/12694189
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]
+then
+	DIR="$PWD"
+fi
+
+source $DIR/ps1
+export PATH=$HOME/bin:$DIR/bin:$HOME/.local/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/.local/lib:$LD_LIBRARY_PATH
+
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+bind -x '"\C-p": vim $(fzf);'
+
+swapcaps 1
